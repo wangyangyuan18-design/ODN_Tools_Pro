cdcc7b85516af2df2c5d503dcc02c2a71bd536a4
wangyangyuan18-design <wangyangyuan18@gmail.com>
2026-09-05 14:24:57 +0800

    Rebuild plugin entry around project management and link design

--- __init__.py
+++ __init__.py
@@ -1,65 +1,72 @@
 # -*- coding: utf-8 -*-
-"""QGIS plugin package entry point for ODN Tools Pro."""
+"""QGIS plugin entry point for ODN Tools Pro."""
+
+import os
 
 
 def classFactory(iface):
-    """Load the ODN Tools Pro plugin for QGIS."""
-    from . import connection_point_engine
-    from . import connection_point_engine_v4
-    connection_point_engine.run_connection_point_naming_v2 = connection_point_engine_v4.run_connection_point_naming_v2
-    from .site_co_design_impl import SiteCoDesign
+    """Load the ODN Tools Pro plugin."""
     from .pole_trace_connect import PoleTraceDialog
     from .overlength_pole import OverlengthPoleDialog
-    from .odn2_planner import Odn2PlannerDialog
-    from .scheme3_manual_link_planner import Scheme3Dialog, Scheme3Engine, Scheme3MapTool
-    from .odn_project import OdnProjectWizard
     from .odn_project_manager import OdnProjectManager, initialize_project_manager_context
-    from .odn_project_config import open_project_config, OdnProjectConfigDialog
+    from .odn_project_config import open_project_config
     from .odn_project_validation import install_validation_page
-    from .scheme3_policy import install_scheme3_policy
+    from .odn_project import OdnProjectWizard
     from .odn_project_integration import install_project_creation_integration
     from .odn_project_config_fix import install_project_config_fix
+    from .scheme3_policy import install_scheme3_policy
     from .scheme3_route_preview_fix import install_scheme3_route_preview_fix
+    from .scheme3_manual_link_planner import Scheme3Dialog, Scheme3Engine, Scheme3MapTool
     from .scheme3_launcher import start_link_design
     from .scheme3_project_ui import install_project_ui_policy
-    from . import odn_project_context as project_context
 
     install_validation_page(OdnProjectWizard)
-    install_scheme3_policy(Scheme3Dialog)
     install_project_creation_integration(OdnProjectWizard)
-    install_project_config_fix(OdnProjectConfigDialog)
+    install_project_config_fix(type(open_project_config).__name__ if False else object)
+    install_scheme3_policy(Scheme3Dialog)
     install_scheme3_route_preview_fix(Scheme3Dialog, Scheme3Engine, Scheme3MapTool)
     install_project_ui_policy(Scheme3Dialog)
     initialize_project_manager_context()
-    import os
 
-    class ODNToolsPro(SiteCoDesign):
-        """Main QGIS plugin controller with ODN planning tools."""
+    class ODNToolsPro:
+        """Main plugin controller for the rebuilt ODN Tools Pro."""
+
+        def __init__(self, iface):
+            self.iface = iface
+            self.plugin_dir = os.path.dirname(os.path.abspath(__file__))
+            self.actions = []
+            self.menu = None
+            self.toolbar = None
 
         def initGui(self):
-            super().initGui()
+            from qgis.PyQt.QtGui import QIcon
+            from qgis.PyQt.QtWidgets import QAction, QMenu
+
             main_window = self.iface.mainWindow()
-            icon_path = os.path.join(self.plugin_dir, 'poleTraceConnect.svg')
-            self.add_action(icon_path, self.tr('项目管理'), self.project_manager, parent=main_window)
-            self.add_action(icon_path, self.tr('项目配置'), self.project_config, parent=main_window)
-            self.add_action(icon_path, self.tr('杆路轨迹自动连线'), self.pole_trace_connect, parent=main_window)
-            self.add_action(icon_path, self.tr('超距增点'), self.overlength_pole, parent=main_window)
-            self.add_action(icon_path, self.tr('ODN 网络规划'), self.fdt_planner, parent=main_window)
-            self.add_action(icon_path, self.tr('链路设计'), self.link_design, parent=main_window)
+            self.menu = QMenu("ODN Tools Pro", main_window)
+            main_window.menuBar().addMenu(self.menu)
+            self.toolbar = self.iface.addToolBar("ODN Tools Pro")
+            self.toolbar.setObjectName("ODNToolsProToolbar")
+
+            self._add("项目管理", self.project_manager)
+            self._add("项目配置", self.project_config)
+            self._add("杆路轨迹自动连线", self.pole_trace_connect)
+            self._add("超距增点", self.overlength_pole)
+            self._add("链路设计", self.link_design)
+
+        def _add(self, text, callback):
+            from qgis.PyQt.QtWidgets import QAction
+            action = QAction(text, self.iface.mainWindow())
+            action.triggered.connect(callback)
+            self.menu.addAction(action)
+            self.toolbar.addAction(action)
+            self.actions.append(action)
 
         def project_manager(self):
-            dialog = OdnProjectManager(self.iface, self.iface.mainWindow())
-            dialog.exec_()
+            OdnProjectManager(self.iface, self.iface.mainWindow()).exec_()
 
         def project_config(self):
-            if not project_context.require_project(self.iface.mainWindow(), '项目配置'):
-                return
             open_project_config(self.iface, self.iface.mainWindow())
-
-        def new_odn_project(self):
-            # Backwards-compatible programmatic entry; the visible UI entry is 项目管理.
-            self._odn_project_dialog = OdnProjectWizard(self.iface, self.iface.mainWindow())
-            self._odn_project_dialog.exec_()
 
         def pole_trace_connect(self):
             PoleTraceDialog(self.iface, self.iface.mainWindow()).exec_()
@@ -67,10 +74,28 @@ def classFactory(iface):
         def overlength_pole(self):
             OverlengthPoleDialog(self.iface, self.iface.mainWindow()).exec_()
 
-        def fdt_planner(self):
-            Odn2PlannerDialog(self.iface, self.iface.mainWindow()).exec_()
-
         def link_design(self):
             self._scheme3_dialog = start_link_design(self.iface, self.iface.mainWindow())
+
+        def unload(self):
+            for action in self.actions:
+                try:
+                    action.deleteLater()
+                except Exception:
+                    pass
+            self.actions.clear()
+            if self.toolbar is not None:
+                try:
+                    self.iface.mainWindow().removeToolBar(self.toolbar)
+                except Exception:
+                    pass
+            self.toolbar = None
+            if self.menu is not None:
+                try:
+                    self.iface.mainWindow().menuBar().removeAction(self.menu.menuAction())
+                    self.menu.deleteLater()
+                except Exception:
+                    pass
+            self.menu = None
 
     return ODNToolsPro(iface)
