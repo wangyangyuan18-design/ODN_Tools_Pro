d95dce00f23723cbc0be890bb4b6dd5fd4592cb7
wangyangyuan18-design <wangyangyuan18@gmail.com>
2026-09-07 18:40:03 +0800

    Activate Link Design v17 route/offset separation

--- __init__.py
+++ __init__.py
@@ -15,7 +15,7 @@ def classFactory(iface):
     from .odn_project_validation import install_validation_page
     from .odn_project import OdnProjectWizard
     from .odn_project_integration import install_project_creation_integration
-    from .link_design_v16 import LinkDesignDock
+    from .link_design_v17 import LinkDesignDock
 
     install_validation_page(OdnProjectWizard)
     install_project_creation_integration(OdnProjectWizard)
