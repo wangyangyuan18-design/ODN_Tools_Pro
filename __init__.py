ca569e979a9332e27357299749e7a1806255ab18
wangyangyuan18-design <wangyangyuan18@gmail.com>
2026-09-07 21:29:38 +0800

    Install coincident FDT FAT landing fallback

--- __init__.py
+++ __init__.py
@@ -19,11 +19,13 @@ def classFactory(iface):
     from .fat_return import install_fat_return_button
     from .plugin_undo import undo_last
     from .offset_write_patch import install_offset_write_patch
+    from .fat_landing_coincident_patch import install_coincident_fat_fallback
 
     install_validation_page(OdnProjectWizard)
     install_project_creation_integration(OdnProjectWizard)
     initialize_project_manager_context()
     install_offset_write_patch()
+    install_coincident_fat_fallback()
 
     class ODNToolsPro:
         def __init__(self, iface):
